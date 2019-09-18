5350e9cfbf0d3b110721a19a23d7e680986a4133
CaleumSeanFoxNorbury <33324819+CaleumSeanFoxNorbury@users.noreply.github.com>
2019-09-18 16:38:19 +0100

    First Graphical component finally outputs a green screen. Will use this screen to output my objects TODO:: ALOT LOL

--- ProjectOne - Graphics/main.cpp
+++ ProjectOne - Graphics/main.cpp
@@ -1,5 +1,18 @@
-
+#include"SDL.h"
+#undef main
 
 int main() {
+	
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* Window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* ren = SDL_CreateRenderer(Window, -1, 0);
+
+	SDL_SetRenderDrawColor(ren, 0, 255, 0, 255);
+
+	SDL_RenderClear(ren);
+
+	SDL_RenderPresent(ren);
+
+	SDL_Delay(3000);
 	return(0);
 }
